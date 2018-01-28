f8c1187f6ea6409cfe3a227ac9832c872bab305b
Mary-Jane McBride <mmcbride37@qub.ac.uk>
2018-01-28 12:47:22 +0000

    Adding chatbot files

--- travelling-tim-chatbot/index.js
+++ travelling-tim-chatbot/index.js
@@ -2,18 +2,54 @@ require('dotenv').load();
 var builder = require('botbuilder');
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 var botMiddleware = require('./middleware');
 
 const app = express();
+app.use('/', express.static('public'));
+
+var savedAddresses = [];
 
 //Create express app
-app.listen(process.env.port || process.env.PORT || 3978, function () {
+app.listen(process.env.port || process.env.PORT || 3979, function () {
     console.log('%s started', app.name);
 });
 
 //express middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+function requestStationScheduleTimeAt(session, station, traveldate) {
+    var request = require('request');
+    traveldate = new Date();
+    traveldate = new Date(traveldate.setHours(20));
+
+    var propertiesObject = { stationName: station, datetime: formatDate(traveldate) };
+    request({url:'http://7bd6b0a6.ngrok.io/api/TrainData/GetTrainAtNearestTime', qs:propertiesObject}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var nexttrain = formatDateToTime(new Date(JSON.parse(body).data.scheduledTime));
+            session.send('There is a train at ' + station + ' scheduled for ' + nexttrain + '.');
+            botMiddleware.delayResponseTyping(session, 2);
+            session.beginDialog('ReceiveNotifications');
+        } else {
+            console.log(error);
+        }
+    })
+}
+
+function requestStationSchedule(session, station) {
+    var request = require('request');
+    var propertiesObject = { stationName: station };
+    request({url:'http://7bd6b0a6.ngrok.io/api/TrainData/GetTrainAtNearestTime', qs:propertiesObject}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(JSON.parse(body).data);
+            var nexttrain = formatDateToTime(new Date(JSON.parse(body).data.scheduledTime));
+            session.send('The next train for ' + session.conversationData.station + ' is scheduled for ' + nexttrain + '.');
+            botMiddleware.delayResponseTyping(session, 2);
+            session.beginDialog('ReceiveNotifications');
+        }
+    })
+}
 
 //Create chatconnector and add app id and password - found on botframework website
 var connector = new builder.ChatConnector({
@@ -86,7 +122,51 @@ bot.dialog('NextTrain', [
         var station = builder.EntityRecognizer.findEntity(args.intent.entities, 'station');
         if (station) {
             console.log(station);
-            session.send('Looking up trains at ' + station.entity);
+            session.send('Looking up trains from ' + station.entity);
+            session.conversationData.station = station.entity;
+            next();
+        } else if (getUsersDefaultStations(session, session.message.address)) {
+            botMiddleware.delayResponseTyping(session, 2);
+            session.beginDialog('GetLocation');
+        } else {
+            botMiddleware.delayResponseTyping(session, 2);
+            sendMessageWithActions('Please specify a station or register to save default stations for next time.');
+        }
+    },
+    function (session, results) {
+        console.log(results);
+        if (results && !session.conversationData.station) {
+            session.conversationData.station = results.response.entity;
+        }
+        botMiddleware.delayResponseTyping(session, 2);
+        requestStationSchedule(session, session.conversationData.station);
+    },
+    function (session, results) {
+        if (results) {
+            session.send('Cool, I\'ll keep you posted if the expected arrival time changes.');
+        }
+    }
+]).triggerAction({
+    matches: 'NextTrain'
+});
+
+bot.dialog('NextTrainAt', [
+    function (session, args, next) {
+        var station = builder.EntityRecognizer.findEntity(args.intent.entities, 'station');
+
+        var departureDate;
+        if (builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.datetimeV2.date')) {
+            departureDate = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.datetimeV2.date');
+        } else if (builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.datetimeV2.datetime')) {
+            departureDate = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.datetimeV2.datetime');
+        }
+        if (departureDate) {
+            session.conversationData.date = departureDate.resolution.values[0].value;
+        }
+
+        if (station) {
+            console.log(station);
+            session.send('Looking up trains from ' + station.entity + ' at ' + session.conversationData.date);
             session.conversationData.station = station.entity;
             next();
         } else if (getUsersDefaultStations(session, session.message.address)) {
@@ -97,26 +177,60 @@ bot.dialog('NextTrain', [
     },
     function (session, results) {
         console.log(results);
-        if (results) {
+        if (results && !session.conversationData.station) {
             session.conversationData.station = results.response.entity;
         }
-        session.send('The next train for ' + session.conversationData.station + ' is scheduled for 4.41pm, however it is expected to arrive 4 minutes late.');
-        session.beginDialog('ReceiveNotifications');
+
+        var traveldate = formatDate(new Date(session.conversationData.date));
+
+        requestStationScheduleTimeAt(session, session.conversationData.station, traveldate);
+
     },
-    function(session, results){
-        if(results){
-            session.send('Cool, I\'ll keep you posted if the expected arrival time changes.');
+    function (session, results) {
+        session.send('Cool, I\'ll keep you posted if the expected arrival time changes.');
+        console.log('ADDRESS: ' + session.message.address);
+        savedAddresses.push(session.message.address);
+        console.log(savedAddresses);
+        session.endDialog();
+    }
+]).triggerAction({
+    matches: 'TrainAt'
+});
+
+bot.dialog('Price', [
+    function (session) {
+        session.send('The price between Botanic Train Station and Lisburn Train Station is £4.00 for a single and £6.20 for a return.');
+        botMiddleware.delayResponseTyping(session, 2);
+        session.send('You can get 1/3 off on your return ticket after 9.30am, which would make it £4.10.');
+        botMiddleware.delayResponseTyping(session, 2);
+        builder.Prompts.choice(session, 'Would you like to purchase one now?', 'Yes|No', { listStyle: builder.ListStyle.button });
+    },
+    function (session, results) {
+        if (results) {
+            session.send('to do - purchasing');
         }
     }
 ]).triggerAction({
-    matches: 'NextTrain'
+    matches: 'Price',
 });
 
+bot.dialog('Bye', function (session) {
+    session.endConversation('Thanks for dropping by!');
+}).triggerAction({
+    matches: 'Goodbye'
+});
+
+bot.dialog('Form', function (session) {
+    session.endDialog('Sure the craic\'s ninety. What can I help you with?');
+}).triggerAction({
+    matches: 'Form'
+})
+
 bot.dialog('ReceiveNotifications', [
-    function(session){
+    function (session) {
         builder.Prompts.text(session, 'Would you like to receive notifications if this train is delayed?', 'Yes|No', { listStyle: builder.ListStyle.button });
     },
-    function(session, results){
+    function (session, results) {
         session.endDialogWithResult(results);
     }
 ])
@@ -125,7 +239,7 @@ bot.dialog('GetLocation', [
     function (session) {
         builder.Prompts.choice(session, 'Assuming you\'re going your usual route, where are you leaving from?', 'Botanic|Lisburn', { listStyle: builder.ListStyle.button });
     },
-    function(session, results){
+    function (session, results) {
         session.endDialogWithResult(results);
     }
 ]);
@@ -141,7 +255,6 @@ function sendMessageWithActions(session, msg) {
         .suggestedActions(
         builder.SuggestedActions.create(
             session, [
-                builder.CardAction.imBack(session, 'onboard', 'New User'),
                 builder.CardAction.imBack(session, 'menu', 'Menu'),
                 builder.CardAction.imBack(session, 'help', 'Help')
             ]
@@ -152,3 +265,55 @@ function getUsersDefaultStations(session, userId) {
     return [builder.CardAction.imBack(session, 'botanic', 'Botanic'),
     builder.CardAction.imBack(session, 'lisburn', 'Lisburn')];
 }
+
+app.post('/api/PushMessage/', (req, res, next) => {
+    var responseText = '';
+    var addresses = [];
+    if (req.body.addresses) {
+        for (var i = 0; i < savedAddresses.length; i++) {
+            for (var c = 0; c < req.body.addresses.length; c++) {
+                if (savedAddresses[i].id == req.body.addresses[c]["key"]) {
+                    addresses.push(savedAddresses[i]);
+                }
+            }
+        }
+    } else {
+        addresses = savedAddresses;
+    }
+    for (var i = 0; i < savedAddresses.length; i++) {
+        var msg = new builder.Message().address(addresses[i]);
+        msg.text(req.body.text);
+        msg.textLocale('en-UK');
+        bot.send(msg);
+        responseText += 'Sent message: ' + req.params.text + ' to ' + addresses[i] + '\r\r';
+    }
+    res.send(responseText);
+    next();
+}
+);
+
+function formatDate(date) {
+    var day = date.getDate();       // yields date
+    var month = date.getMonth() + 1;    // yields month (add one as '.getMonth()' is zero indexed)
+    var year = date.getFullYear();  // yields year
+    var hour = date.getHours();     // yields hours 
+    var minute = date.getMinutes(); // yields minutes
+    var second = date.getSeconds(); // yields seconds
+    
+    // After this construct a string with the above results as below
+    var time = day + "/" + month + "/" + year + " " + hour + ':' + minute + ':' + second; 
+    return time;
+  }
+
+  function formatDateToTime(date){
+    var day = date.getDate();       // yields date
+    var month = date.getMonth() + 1;    // yields month (add one as '.getMonth()' is zero indexed)
+    var year = date.getFullYear();  // yields year
+    var hour = date.getHours();     // yields hours 
+    var minute = date.getMinutes(); // yields minutes
+    var second = date.getSeconds(); // yields seconds
+    
+    // After this construct a string with the above results as below
+    var time = hour + ':' + minute; 
+    return time;
+  }
